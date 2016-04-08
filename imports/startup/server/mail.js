17c229810c3058f1ce69f48d9a77d3acf45f7b64
Jacob Mischka <jacobmischka@gmail.com>
2016-04-08 16:16:03 -0500

    Wow I did nothing today huh.

--- imports/startup/server/mail.js
+++ imports/startup/server/mail.js
@@ -3,8 +3,9 @@ const password = "18d0ca555fd201";
 const host = "mailtrap.io";
 const port = "2525";
 
-export const MAIL_URL = `smtp://${username}:${password}@${host}:${port}/`
+const MAIL_URL = `smtp://${username}:${password}@${host}:${port}/`;
 
 Meteor.startup(() => {
-	process.env.MAIL_URL = MAIL_URL;
+	if(!process.env.MAIL_URL)
+		process.env.MAIL_URL = MAIL_URL;
 });
