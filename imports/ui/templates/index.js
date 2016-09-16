59fbb4ab4aa64953facee2409b96cc05de799281
Jacob Mischka <jacobmischka@gmail.com>
2016-09-16 16:10:45 -0500

    Add calendar view, some styling

--- imports/ui/templates/index.js
+++ imports/ui/templates/index.js
@@ -1,5 +1,7 @@
+import './calendar.js';
 import './home.js';
 import './requests.js';
+import './requestsList.js';
 import './usersList.js';
 import './locationsList.js';
 import './fellowshipsList.js';
