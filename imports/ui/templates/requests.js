7ef03bd9c83c9d04a45d03be4ccad1c1201e4f9f
Jacob Mischka <jacobmischka@gmail.com>
2016-09-16 23:11:11 -0500

    Styling, tabs for requests page

--- imports/ui/templates/requests.js
+++ imports/ui/templates/requests.js
@@ -1,10 +1,23 @@
 import './requests.html';
 
+let views = [
+	'#table-view',
+	'#calendar-view'
+];
+
 Template.requests.onRendered(function(){
-	Session.set('requestsView', window.location.hash);
+	if(window.location.hash.length > 0 && views.includes(window.location.hash))
+		Session.set('requestsView', window.location.hash);
+
+	if(!Session.get('requestsView'))
+		Session.set('requestsView', '#table-view');
 });
 
 Template.requests.helpers({
+	isActiveView(view){
+		if(Session.equals('requestsView', view))
+			return 'active';
+	},
 	showCalendar(){
 		return Session.equals('requestsView', '#calendar-view');
 	}
@@ -13,6 +26,7 @@ Template.requests.helpers({
 Template.requests.events({
 	'click .view-link'(event){
 		let hash = event.target.getAttribute('href');
-		Session.set('requestsView', hash);
+		if(views.includes(hash))
+			Session.set('requestsView', hash);
 	}
 });
