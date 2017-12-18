752d181a4ca441645def7570ab2ba5a187bc39d6
Jacob Mischka <jacob@mischka.me>
2017-12-18 15:13:21 -0600

    Update deps, fix `React is not defined` errors
    It was because of a babel plugin that shouldn't have been there, I guess
    that's handled elsewhere in new versions of Meteor. Idk. Wasted many
    hours to discover that.

--- imports/ui/components/RequestDetails.js
+++ imports/ui/components/RequestDetails.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import find from 'lodash/find';
 import moment from 'moment';
 import { throwError } from 'meteor/saucecode:rollbar';
--- imports/ui/components/RequestsStatsTable.js
+++ imports/ui/components/RequestsStatsTable.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
 import moment from 'moment';
 import 'twix';
