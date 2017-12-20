72c6e29300999d245203e2147b92c87cf6ac0426
Jacob Mischka <jacob@mischka.me>
2017-12-20 07:57:21 -0600

    Barely start on homepage react rewrite

---
+++ imports/ui/components/Home.js
@@ -0,0 +1,18 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+
+export default class Home extends Component<{}, {}> {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+	
+	requestedLocation = () => {
+		
+	}
+}
