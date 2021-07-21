109b6992006bda53ea6bc818f2d0cd2102ad6bc3
Jacob Mischka <jacob@mischka.me>
2021-07-21 09:01:00 -0500

    Specify ADMIN_EMAIL_ADDRESS via env instead of hardcoding it

--- imports/constants.js
+++ imports/constants.js
@@ -2,9 +2,12 @@ export const APP_SITE_NAME = 'dayoff.site';
 export const APP_ADMIN_EMAIL_ADDRESS = 'admin@dayoff.site';
 export const APP_ACCOUNTS_EMAIL_ADDRESS = 'accounts@dayoff.site';
 export const APP_NOTIFICATION_EMAIL_ADDRESS = 'notification@dayoff.site';
-export const ADMIN_EMAIL_ADDRESS = 'jmischka@mcw.edu';
 export const DAYS_BEFORE_REQUEST_TO_SEND_REMINDER = 5;
 export const ISO_DATE_FORMAT = 'Y-MM-DD';
+
+const { ADMIN_EMAIL_ADDRESS = 'admin@dayoff.site' } = process.env;
+
+export { ADMIN_EMAIL_ADDRESS };
 
 export const SCREEN_BREAKPOINTS = {
 	SMALL_MOBILE: 480,
